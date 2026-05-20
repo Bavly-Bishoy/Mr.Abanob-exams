1f272c6f05fc4717772c82c43169b50e117bcabd
Bavly-Bishoy <bavlybishoy01@gmail.com>
2026-05-20 18:54:50 +0300

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -3,10 +3,10 @@ import { getDatabase, ref, push, set, get, child } from "https://www.gstatic.com
 
 const firebaseConfig = {
   apiKey: "AIzaSyAVFxlp7aXIuIKiq9ySeyE4d6R-a4WLVGc",
-  authDomain: "bavly-exams.firebaseapp.com",
-  databaseURL: "https://bavly-exams-default-rtdb.firebaseio.com",
-  projectId: "bavly-exams",
-  storageBucket: "bavly-exams.firebasestorage.app",
+  authDomain: "mr-abanob-exams.firebaseapp.com",
+  databaseURL: "https://mr-abanob-exams-default-rtdb.firebaseio.com",
+  projectId: "mr-abanob-exams",
+  storageBucket: "mr-abanob-exams.firebasestorage.app",
   messagingSenderId: "295662640771",
   appId: "1:295662640771:web:115931a29a8a1032c545b6",
   measurementId: "G-9KEQ1YL5NX"
